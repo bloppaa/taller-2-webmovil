1d0a02cd2e1929dc49927e42f6b72df279f3dc15
Pablo <pablocortes1101@gmail.com>
2025-11-16 23:51:40 -0300

    feat: agregar apk

--- api-fastapi/main.py
+++ api-fastapi/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.games import router as games_router
+import os
+import uvicorn
 
 app = FastAPI()
 app.add_middleware(
@@ -11,3 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(games_router, prefix="/api")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
