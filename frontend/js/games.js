082034a8530889486ca2f8843a009dc079a70026
Pablo <pablocortes1101@gmail.com>
2025-11-16 22:21:36 -0300

    feat: listo el frontend

--- frontend/js/games.js
+++ frontend/js/games.js
@@ -2,10 +2,38 @@ const gamesContainer = document.getElementById("games");
 const searchInput = document.getElementById("search");
 const orderbySelect = document.getElementById("orderby");
 
+const platformIcons = {
+  PlayStation: '<i class="fab fa-playstation"></i>',
+  Xbox: '<i class="fab fa-xbox"></i>',
+  PC: '<i class="fab fa-windows"></i>',
+  "Apple Machintosh": '<i class="fab fa-apple"></i>',
+  iOS: '<i class="fab fa-apple"></i>',
+  Android: '<i class="fab fa-android"></i>',
+  Linux: '<i class="fab fa-linux"></i>',
+  Nintendo: '<i class="fas fa-n"></i>',
+};
+
+function getPlatformIcons(platforms) {
+  return platforms
+    .split(",")
+    .map((platform) => platformIcons[platform] || "")
+    .join(" ");
+}
+
+function formatDate(dateString) {
+  if (!dateString) return "N/A";
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+}
+
 async function fetchGames(search = "", orderby = "") {
   const params = new URLSearchParams();
   if (search) params.append("search", search);
-  if (orderby) params.append("orderby", orderby);
+  if (orderby && orderby !== "relevance") params.append("orderby", orderby);
   params.append("limit", 20);
 
   const response = await fetch(
@@ -20,25 +48,49 @@ function displayGames(games) {
   games.forEach((game) => {
     const gameCard = document.createElement("div");
     gameCard.classList.add(
-      "bg-white",
+      "bg-zinc-800",
       "rounded-lg",
       "shadow-md",
       "overflow-hidden",
       "transform",
       "transition",
-      "hover:scale-105"
+      "hover:scale-105",
+      "text-white"
     );
+    let metacriticColor = "";
+    if (game.metacritic >= 75) {
+      metacriticColor = "text-lime-500";
+    } else if (game.metacritic >= 50) {
+      metacriticColor = "text-yellow-500";
+    } else if (game.metacritic < 50) {
+      metacriticColor = "text-red-500";
+    }
+
+    const platforms = getPlatformIcons(game.platforms || []);
+
+    const metacriticDiv = game.metacritic
+      ? `<div class="font-bold ${metacriticColor} border py-0.5 flex items-center px-1.5 rounded text-sm">${game.metacritic}</div>`
+      : "";
 
     gameCard.innerHTML = `
-      <img src="${game.image || "https://via.placeholder.com/250"}" alt="${
-      game.name
-    }" class="w-full h-40 object-cover">
+      <img src="${game.image}" alt="${game.name}" class="w-full object-cover">
       <div class="p-4">
+        <div class="flex justify-between items-center mb-2">
+          <span class="flex gap-1.5">${platforms}</span>
+          ${metacriticDiv}
+        </div>
         <h3 class="text-lg font-bold mb-2">${game.name}</h3>
-        <p class="text-sm text-gray-600">Released: ${game.released || "N/A"}</p>
-        <p class="text-sm text-gray-600">Metacritic: ${
-          game.metacritic || "N/A"
-        }</p>
+        <div class="flex justify-between">
+          <p class="text-xs text-neutral-500">Release date:</p>
+          <p class="text-xs ml-1">${formatDate(game.released)}</p>
+        </div>
+        <hr class="my-4 text-neutral-700" />
+        <div class="flex justify-between">
+          <p class="text-xs text-neutral-500">Genres:</p>
+          <p class="w-1/2 flex justify-end-safe text-xs ml-1">${game.genres
+            .split(",")
+            .join(", ")}</p>
+        </div>
       </div>
     `;
 
